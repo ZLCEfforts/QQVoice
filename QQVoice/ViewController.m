9fbbe1648269f1ca1487900a5e2d84d4a39063bc
“291585089@qq.com” <291585089@qq.com>
2018-02-07 14:52:41 +0800

    第一次提交

---
+++ QQVoice/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  QQVoice
+//
+//  Created by mac on 2018/2/7.
+//  Copyright © 2018年 QQVoiceDemo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+   
+}
+
+
+
+@end
